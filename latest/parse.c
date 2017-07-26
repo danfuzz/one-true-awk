03963332b5e70bc891211b4b22d50abcbaf73c95
Dan Bornstein <danfuzz@milk.com>
2017-07-25 21:05:02 -0700

    1993-07-23. (History generated via build-latest)

--- latest/parse.c
+++ latest/parse.c
@@ -1,76 +1,90 @@
-/*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
- *
- * This module is believed to contain source code proprietary to AT&T.
- * Use and redistribution is subject to the Berkeley Software License
- * Agreement and your Software Agreement with AT&T (Western Electric).
- */
+/****************************************************************
+Copyright (C) AT&T 1993
+All Rights Reserved
 
-#ifndef lint
-static char sccsid[] = "@(#)parse.c	4.3 (Berkeley) 4/17/91";
-#endif /* not lint */
+Permission to use, copy, modify, and distribute this software and
+its documentation for any purpose and without fee is hereby
+granted, provided that the above copyright notice appear in all
+copies and that both that the copyright notice and this
+permission notice and warranty disclaimer appear in supporting
+documentation, and that the name of AT&T or any of its entities
+not be used in advertising or publicity pertaining to
+distribution of the software without specific, written prior
+permission.
 
-#include "awk.def"
+AT&T DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+IN NO EVENT SHALL AT&T OR ANY OF ITS ENTITIES BE LIABLE FOR ANY
+SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+THIS SOFTWARE.
+****************************************************************/
+
+#define DEBUG
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "awk.h"
-#include "stdio.h"
-node *ALLOC(n)
+#include "y.tab.h"
+
+Node *nodealloc(int n)
 {
-	register node *x;
-	x = (node *) malloc(sizeof(node) + (n-1)*sizeof(node *));
+	Node *x;
+
+	x = (Node *) malloc(sizeof(Node) + (n-1)*sizeof(Node *));
 	if (x == NULL)
-		error(FATAL, "out of space in ALLOC");
+		ERROR "out of space in nodealloc" FATAL;
+	x->nnext = NULL;
+	x->lineno = lineno;
 	return(x);
 }
-node *exptostat(a) node *a;
+
+Node *exptostat(Node *a)
 {
 	a->ntype = NSTAT;
 	return(a);
 }
-node	*nullstat;
-node *node0(a)
+
+Node *node1(int a, Node *b)
 {
-	register node *x;
-	x=ALLOC(0);
-	x->nnext = NULL;
-	x->nobj=a;
-	return(x);
-}
-node *node1(a,b) node *b;
-{
-	register node *x;
-	x=ALLOC(1);
-	x->nnext = NULL;
-	x->nobj=a;
+	Node *x;
+
+	x = nodealloc(1);
+	x->nobj = a;
 	x->narg[0]=b;
 	return(x);
 }
-node *node2(a,b,c) node *b, *c;
+
+Node *node2(int a, Node *b, Node *c)
 {
-	register node *x;
-	x = ALLOC(2);
-	x->nnext = NULL;
+	Node *x;
+
+	x = nodealloc(2);
 	x->nobj = a;
 	x->narg[0] = b;
 	x->narg[1] = c;
 	return(x);
 }
-node *node3(a,b,c,d) node *b, *c, *d;
+
+Node *node3(int a, Node *b, Node *c, Node *d)
 {
-	register node *x;
-	x = ALLOC(3);
-	x->nnext = NULL;
+	Node *x;
+
+	x = nodealloc(3);
 	x->nobj = a;
 	x->narg[0] = b;
 	x->narg[1] = c;
 	x->narg[2] = d;
 	return(x);
 }
-node *node4(a,b,c,d,e) node *b, *c, *d, *e;
+
+Node *node4(int a, Node *b, Node *c, Node *d, Node *e)
 {
-	register node *x;
-	x = ALLOC(4);
-	x->nnext = NULL;
+	Node *x;
+
+	x = nodealloc(4);
 	x->nobj = a;
 	x->narg[0] = b;
 	x->narg[1] = c;
@@ -78,82 +92,164 @@ node *node4(a,b,c,d,e) node *b, *c, *d, *e;
 	x->narg[3] = e;
 	return(x);
 }
-node *stat3(a,b,c,d) node *b, *c, *d;
+
+Node *stat1(int a, Node *b)
 {
-	register node *x;
-	x = node3(a,b,c,d);
-	x->ntype = NSTAT;
-	return(x);
-}
-node *op2(a,b,c) node *b, *c;
-{
-	register node *x;
-	x = node2(a,b,c);
-	x->ntype = NEXPR;
-	return(x);
-}
-node *op1(a,b) node *b;
-{
-	register node *x;
-	x = node1(a,b);
-	x->ntype = NEXPR;
-	return(x);
-}
-node *stat1(a,b) node *b;
-{
-	register node *x;
+	Node *x;
+
 	x = node1(a,b);
 	x->ntype = NSTAT;
 	return(x);
 }
-node *op3(a,b,c,d) node *b, *c, *d;
+
+Node *stat2(int a, Node *b, Node *c)
 {
-	register node *x;
-	x = node3(a,b,c,d);
-	x->ntype = NEXPR;
-	return(x);
-}
-node *stat2(a,b,c) node *b, *c;
-{
-	register node *x;
+	Node *x;
+
 	x = node2(a,b,c);
 	x->ntype = NSTAT;
 	return(x);
 }
-node *stat4(a,b,c,d,e) node *b, *c, *d, *e;
+
+Node *stat3(int a, Node *b, Node *c, Node *d)
 {
-	register node *x;
+	Node *x;
+
+	x = node3(a,b,c,d);
+	x->ntype = NSTAT;
+	return(x);
+}
+
+Node *stat4(int a, Node *b, Node *c, Node *d, Node *e)
+{
+	Node *x;
+
 	x = node4(a,b,c,d,e);
 	x->ntype = NSTAT;
 	return(x);
 }
-node *valtonode(a, b) cell *a;
+
+Node *op1(int a, Node *b)
 {
-	register node *x;
-	x = node0(a);
+	Node *x;
+
+	x = node1(a,b);
+	x->ntype = NEXPR;
+	return(x);
+}
+
+Node *op2(int a, Node *b, Node *c)
+{
+	Node *x;
+
+	x = node2(a,b,c);
+	x->ntype = NEXPR;
+	return(x);
+}
+
+Node *op3(int a, Node *b, Node *c, Node *d)
+{
+	Node *x;
+
+	x = node3(a,b,c,d);
+	x->ntype = NEXPR;
+	return(x);
+}
+
+Node *op4(int a, Node *b, Node *c, Node *d, Node *e)
+{
+	Node *x;
+
+	x = node4(a,b,c,d,e);
+	x->ntype = NEXPR;
+	return(x);
+}
+
+Node *valtonode(Cell *a, int b)
+{
+	Node *x;
+
+	a->ctype = OCELL;
+	a->csub = b;
+	x = node1(0, (Node *) a);
 	x->ntype = NVALUE;
-	x->subtype = b;
 	return(x);
 }
-node *pa2stat(a,b,c) node *a, *b, *c;
+
+Node *rectonode(void)	/* make $0 into a Node */
 {
-	register node *x;
-	x = node3(paircnt++, a, b, c);
-	x->ntype = NPA2;
+	return valtonode(recloc, CFLD);
+}
+
+Node *makearr(Node *p)
+{
+	Cell *cp;
+
+	if (isvalue(p)) {
+		cp = (Cell *) (p->narg[0]);
+		if (isfunc(cp))
+			ERROR "%s is a function, not an array", cp->nval SYNTAX;
+		else if (!isarr(cp)) {
+			xfree(cp->sval);
+			cp->sval = (uchar *) makesymtab(NSYMTAB);
+			cp->tval = ARR;
+		}
+	}
+	return p;
+}
+
+Node *pa2stat(Node *a, Node *b, Node *c)	/* pat, pat {...} */
+{
+	Node *x;
+
+	x = node4(PASTAT2, a, b, c, (Node *) paircnt);
+	paircnt++;
+	x->ntype = NSTAT;
 	return(x);
 }
-node *linkum(a,b) node *a, *b;
+
+Node *linkum(Node *a, Node *b)
 {
-	register node *c;
-	if(a == NULL) return(b);
-	else if(b == NULL) return(a);
-	for(c=a; c->nnext != NULL; c=c->nnext);
+	Node *c;
+
+	if (errorflag)	/* don't link things that are wrong */
+		return a;
+	if (a == NULL)
+		return(b);
+	else if (b == NULL)
+		return(a);
+	for (c = a; c->nnext != NULL; c = c->nnext)
+		;
 	c->nnext = b;
 	return(a);
 }
-node *genprint()
-{
-	register node *x;
-	x = stat2(PRINT,valtonode(lookup("$record", symtab, 0), CFLD), nullstat);
-	return(x);
+
+void defn(Cell *v, Node *vl, Node *st)	/* turn on FCN bit in definition, */
+{					/*   body of function, arglist */
+	Node *p;
+	int n;
+
+	if (isarr(v)) {
+		ERROR "`%s' is an array name and a function name", v->nval SYNTAX;
+		return;
+	}
+	v->tval = FCN;
+	v->sval = (uchar *) st;
+	n = 0;	/* count arguments */
+	for (p = vl; p; p = p->nnext)
+		n++;
+	v->fval = n;
+	dprintf( ("defining func %s (%d args)\n", v->nval, n) );
+}
+
+isarg(uchar *s)		/* is s in argument list for current function? */
+{			/* return -1 if not, otherwise arg # */
+	extern Node *arglist;
+	Node *p = arglist;
+	int n;
+
+	for (n = 0; p != 0; p = p->nnext, n++)
+		if (strcmp(((Cell *)(p->narg[0]))->nval, s) == 0)
+			return n;
+	return -1;
 }
